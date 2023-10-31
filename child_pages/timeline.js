b777b788d817ac16037d63087c41e7d9ae3297bf
Steven Solof <stebssbets@yahoo.com>
2023-10-31 19:51:35 -0400

    scrapping complicated timeline and trying simpler one

--- child_pages/timeline.js
+++ child_pages/timeline.js
@@ -1,41 +1,19 @@
-$(document).ready(function () {
-    var mySwiper = new Swiper(".swiper", {
-      autoHeight: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-      },
-      speed: 500,
-      direction: "horizontal",
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar"
-      },
-      loop: false,
-      effect: "slide",
-      spaceBetween: 30,
-      on: {
-        init: function () {
-          $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
-          $(".swiper-pagination-custom .swiper-pagination-switch").eq(0).addClass("active");
-        },
-        slideChangeTransitionStart: function () {
-          $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
-          $(".swiper-pagination-custom .swiper-pagination-switch").eq(mySwiper.realIndex).addClass("active");
-
-          $(".swiper-slide .title").removeClass("visible");
-          $(".swiper-slide-active .title").addClass("visible");
-        }
-      }
-    });
-    $(".swiper-pagination-custom .swiper-pagination-switch").click(function () {
-      mySwiper.slideTo($(this).index());
-      $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
-      $(this).addClass("active");
-    });
-  });
+function displayEventInfo(eventId) {
+  const eventInfo = document.getElementById('eventInfo');
+  switch (eventId) {
+      case 'event1':
+          eventInfo.innerHTML = "Details for Event 1.";
+          break;
+      case 'event2':
+          eventInfo.innerHTML = "Details for Event 2.";
+          break;
+      case 'event3':
+          eventInfo.innerHTML = "Details for Event 3.";
+          break;
+      // Add more cases as needed
+      default:
+          eventInfo.innerHTML = "";
+          break;
+  }
+}
   
