5558abc544f6e8890e3b96710ac1f7768c9314e3
Justin <pierrelampre@gmail.com>
2024-05-01 20:01:37 -0400

    homepage nav rewrite and addition of metatags for site preview image

--- app.js
+++ app.js
@@ -73,6 +73,31 @@ function cleanEvents() {
 
 cleanEvents();
 
+
+//show and hide elections nav section
+
+let electionsNav = document.querySelector('.elections-dropdown-links');
+let hoverAction = document.getElementById('navBoxToggle');
+
+
+
+hoverAction.onmouseover = function () {
+    electionsNav.style.display = "flex";
+}
+
+hoverAction.onmouseout = function () {
+    electionsNav.style.display = "none";
+}
+
+electionsNav.onmouseover = function () {
+    electionsNav.style.display = "flex";
+}
+
+electionsNav.onmouseout = function () {
+    electionsNav.style.display = "none";
+}
+
+
 //Modal Script
 
 let theModal = document.getElementById('myModal');
@@ -86,13 +111,20 @@ window.onload = () => {
 closeModalButton.onclick = () => {
     theModal.close();
     theModal.style.display = 'none';
-    quickSummaryButton.style.display = 'grid';
+    if (quickSummaryButton !== null) {
+        quickSummaryButton.style.display = 'grid';
+    }
 }
 
+
+
 quickSummaryButton.onclick = () => {
-    quickSummaryButton.style.display = 'none';
+    if (quickSummaryButton !== null) {
+        quickSummaryButton.style.display = 'none';
+    }
     theModal.style.display = 'grid';
     theModal.showModal();
 }
+
 
 
