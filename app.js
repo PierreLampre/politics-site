2a2b92f6c70a7f1da3912e515554ff6c2b03c3e8
Stebs <stebssbets@yahoo.com>
2024-09-06 14:07:18 -0400

    save untracked files before pull

---
+++ app.js
@@ -0,0 +1,110 @@
+console.log("linked");
+
+const date = new Date();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let year = date.getFullYear();
+
+//remove events in a past month or day 
+
+function cleanEvents() {
+
+    let htmlCollection = document.getElementsByClassName('date-and-time');
+
+    let arrayOfLis = Array.from(htmlCollection);
+
+    for (let i = 0; i < arrayOfLis.length; i++) {
+        let event = arrayOfLis[i];
+        let eventMonth = event.textContent.slice(0, 2);
+
+        function wipeIt(event) {
+            console.log(event);
+            let dad = event.parentElement;
+            let grandpa = dad.parentElement;
+            grandpa.remove();
+        }
+
+        if (eventMonth.slice(0, 1) == 0) {
+            eventMonth = eventMonth.slice(1, 2);
+
+            eventMonth = parseInt(eventMonth);
+            month = parseInt(month);
+
+            if (eventMonth < month) {
+                wipeIt(event);
+            } else if (eventMonth == month) {
+                let eventDay = event.textContent.slice(3, 5);
+                if (parseInt(eventDay.slice(0, 1)) == 0) {
+                    eventDay = parseInt(event.textContent.slice(1, 2));
+                    day = parseInt(day);
+                    console.log(day)
+                    if (eventDay < day) {
+                        wipeIt(event);
+                    }
+                } else if (eventDay < day) {
+                    wipeIt(event);
+                }
+            }
+        } else {
+            eventMonth = eventMonth.slice(0, 2);
+
+            eventMonth = parseInt(eventMonth);
+            month = parseInt(month);
+
+            if (eventMonth < month) {
+                wipeIt(event);
+            } else if (eventMonth == month) {
+                let eventDay = event.textContent.slice(3, 5);
+                if (parseInt(eventDay.slice(0, 1)) == 0) {
+                    eventDay = parseInt(event.textContent.slice(1, 2));
+                    day = parseInt(day);
+                    console.log(day)
+                    if (eventDay < day) {
+                        wipeIt(event);
+                    }
+                } else if (eventDay < day) {
+                    wipeIt(event);
+                }
+            }
+        }
+
+    }
+}
+
+cleanEvents();
+
+
+//Common Modal Script
+
+let theModal = document.getElementById('myModal');
+let closeModalButton = document.querySelector('.close');
+let quickSummaryButton = document.querySelector('.quick-summary-reopen');
+
+//if a quiek summary button exists, display it
+
+window.onload = () => {
+    if (quickSummaryButton !== null) {
+        quickSummaryButton.style.display = 'grid';
+    }
+}
+
+closeModalButton.onclick = () => {
+    theModal.close();
+    theModal.style.display = 'none';
+    if (quickSummaryButton !== null) {
+        quickSummaryButton.style.display = 'grid';
+    }
+}
+
+
+
+quickSummaryButton.onclick = () => {
+    if (quickSummaryButton !== null) {
+        quickSummaryButton.style.display = 'none';
+    }
+    theModal.style.display = 'grid';
+    theModal.showModal();
+}
+
+
+
---
+++ homepage_scripts.js
@@ -0,0 +1,74 @@
+//show and hide elections nav section
+
+let electionsNav = document.querySelector('.elections-dropdown-links');
+let hoverAction = document.getElementById('navBoxToggle');
+let countyNav = document.querySelector('.county-dropdown-links');
+let hoverAction2 = document.getElementById('navBoxToggle2');
+
+
+
+hoverAction.onmouseover = function () {
+    electionsNav.style.display = "flex";
+}
+
+hoverAction.onmouseout = function () {
+    electionsNav.style.display = "none";
+}
+
+electionsNav.onmouseover = function () {
+    electionsNav.style.display = "flex";
+}
+
+electionsNav.onmouseout = function () {
+    electionsNav.style.display = "none";
+}
+//next dropdown
+
+hoverAction2.onmouseover = function () {
+    countyNav.style.display = "flex";
+}
+
+hoverAction2.onmouseout = function () {
+    countyNav.style.display = "none";
+}
+
+countyNav.onmouseover = function () {
+    countyNav.style.display = "flex";
+}
+
+countyNav.onmouseout = function () {
+    countyNav.style.display = "none";
+}
+
+//Homepage dual modal options for banner
+
+//Common Modal Script
+
+let Modal1 = document.getElementById('Modal1');
+let Modal2 = document.getElementById('Modal2');
+let videoSummary = document.getElementById('videoSummary');
+let actualVideo = document.getElementById('actualVideo');
+let close1 = document.getElementById('close1');
+let close2 = document.getElementById('close2');
+
+close1.onclick = () => {
+    Modal1.close();
+    Modal1.style.display = 'none';
+}
+
+close2.onclick = () => {
+    Modal2.close();
+    Modal2.style.display = 'none';
+}
+
+videoSummary.onclick = () => {
+
+    Modal1.style.display = 'grid';
+    Modal1.showModal();
+}
+
+actualVideo.onclick = () => {
+
+    Modal2.style.display = 'grid';
+    Modal2.showModal();
+}
